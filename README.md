9fb17fd606705d24b9dee4e3320ae2278d8d0681
dev soft <dev.soft687@gmail.com>
2025-04-26 01:45:17 +0530

    improvement

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Task 33: Cookies and response
+
+**Regsiter Page**
+![Register Page](image.png)
+
+**Login Page**
+![Login Page](image-1.png)
+
+**Profile Page**
+
+![Profile Page](image-2.png)
--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router'
 import Register from './components/Register'
 import Login from './components/Login'
 import Profile from './components/Profile'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   
 
   return (
     <>
-    <ToastContainer />
+    
       <Router>
         <Routes>
             <Route path="/" element={<Navigate to="/login"/>} />
